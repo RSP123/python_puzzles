55aa18f257f19c825360bc5370d1551d31023d9a
Jayanth Nagaraj <jayanth.s5680@gmail.com>
2019-06-11 18:24:41 +0530

    Changed few things for code quality and corrections

--- circle_circumference.py
+++ circle_circumference.py
@@ -1,16 +1,14 @@
 #This program print the circumference and area of the circle
-#START
 PI = 3.14
+
 #Function to calculate the circumference
 def circumference_circle(radius):
     ci = PI * 2 * radius
-    #Prints the circumference
     return ci
 
 #Function to calculate the area
 def area_circle(radius):
     area = PI * radius * radius
-    #Prints the area
     return area
 
 #Main Function
@@ -19,12 +17,10 @@ def main():
     area_circle(radius)
 
 
-
-
 if __name__ == "__main__" :
-    main()
     radius = float(input("Enter the Radius of circle >> "))
     ci=circumference_circle(radius)
     area=area_circle(radius)
+    
     #Prints the values of circumference and area of circle
     print("Circumference of Cirlce is %.5f:" %ci, "Area of circle is :%f" %area )
