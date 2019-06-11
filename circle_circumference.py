5a35dac0c89a473987d0ad923ac873b02848ffad
Jayanth Nagaraj <jayanth.s5680@gmail.com>
2019-06-11 18:33:59 +0530

    Changed few things for learning purpose

--- circle_circumference.py
+++ circle_circumference.py
@@ -13,15 +13,18 @@ def area_circle(radius):
 
 #Main Function
 def main():
+    # This function can be called from  another program to know the \
+    # flow of the program
     radius = float(input("Enter the Radius of circle >> "))
     ci=circumference_circle(radius)
     area=area_circle(radius)
 
-    #Prints the values of circumference and area of circle
     print("Circumference of Cirlce is %.5f:" %ci, "Area of circle is :%f" %area )
 
 
 if __name__ == "__main__" :
+    # This block executes if this program is dicrectly called or executed by the user
+    
     radius = float(input("Enter the Radius of circle >> "))
     ci=circumference_circle(radius)
     area=area_circle(radius)
