1700a3ef02c089d719355710cb47d94337ea935f
rsp123 <pradeep.rs@hotmail.com>
2019-06-12 12:54:46 +0530

    Changed code for all test cases

--- prime_num.py
+++ prime_num.py
@@ -1,18 +1,23 @@
-#This program check weather the given number is prime or not
-#Function to check prime number
+# This program check weather the given number is prime or not
+# Function to check prime number
 def prime_number(number):
-    if number%2 == 0:
-        #When condition is true
-        return False
-    else:
-        #When condition is false
-        return True
+	# Checking number equals to 1
+	if (number == 1)!=(number < 0):
+		return False
+	for i in range(2, number):
+	   	if (number % i) == 0:
+	    	# When condition is true
+		   	return False
+	else:
+        	# When condition is false
+			return True
 
-#Main Function
+# Main Function
 if __name__ == '__main__':
-    #Getting a number from the user
-    number=int(input("Enter a number to check weather it is prime or not: "))
-    if prime_number(number):
-        print("The given number => ", number, " is a prime number")
-    else:
-        print("The given number => ", number, " is not a prime number")
+	# Getting a number from the user
+	number=int(input("Enter a number to check weather it is prime or not: "))
+    # Input from user
+	if prime_number(number):
+		print("The given number => ", number, " is a prime number")
+	else:
+	    print("The given number => ", number, " is not a prime number")
