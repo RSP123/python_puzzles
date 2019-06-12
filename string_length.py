01efe1e06d58134c93ee91fc42f0c08359beff46
rsp123 <pradeep.rs@hotmail.com>
2019-06-12 17:20:02 +0530

    Length of string

---
+++ string_length.py
@@ -0,0 +1,15 @@
+# This program prints the lenght of the given string
+
+# Function for the finding length
+
+def string_function(string):
+    for i in string:
+        # Calculating length of string
+        len = len + 1
+    return len
+
+
+
+if __name__ == "__main__":
+    string = input("Enter a string here > ")    # User input
+    print("Given string is : ", string,"Length is : ", string_function(string))
