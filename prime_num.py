b799b86c8e6b4f96d8f987d716db99eb47f6f3ec
Jayanth Nagaraj <jayanth.s5680@gmail.com>
2019-06-12 13:09:55 +0530

    changed if confition in line number 5

--- prime_num.py
+++ prime_num.py
@@ -2,8 +2,9 @@
 # Function to check prime number
 def prime_number(number):
 	# Checking number equals to 1
-	if (number == 1)!=(number < 0):
+	if number == 1 or number < 0:
 		return False
+
 	for i in range(2, number):
 	   	if (number % i) == 0:
 	    	# When condition is true
