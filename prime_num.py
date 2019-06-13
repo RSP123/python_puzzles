187b255e5699407b3b3f1eb9befd34b6a896a410
rsp123 <pradeep.rs@hotmail.com>
2019-06-13 15:32:14 +0530

    Final Enhanced code

--- prime_num.py
+++ prime_num.py
@@ -3,28 +3,31 @@
 
 def prime_number(number):
 	# Returning False for number 1 and less then 1
-	if number == 1 or number < 0:
+	if number == 2:
+		return True
+	elif number <= 1 or number%2 == 0:
 		return False
-
-	# Iterating from 2 to check the number is prime or not
-	for i in range(2, number):
-		print(i)
-		if (number % i) == 0:
-		# Returns False if the number is even
-			return False
-		else:
-			return True
+	else:
+		# Iterating from 2 to check the number is prime or not
+		for i in range(3, int((number/2))+1, 2):
+			if (number%i) == 0:
+			    return False
+	return True
 
 
+# Main function
 def main():
-	# Main function
 	number=int(input("Enter a number to check weather it is prime or not: "))
+
+	if number == 1:
+		print("It's neither prime nor composite !!!")
+		return None
 	if prime_number(number):
 		print("The given number => ", number, " is a prime number")
 	else:
 	    print("The given number => ", number, " is not a prime number")
 
+
 # Checks the program call name
 if __name__ == '__main__':
-	# NOTE: This block runs when the user compiles this file only ***
 	main()
