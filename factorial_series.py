051e0d70214e6bc75a9430eeb4bfffdcb22de20f
rsp123 <pradeep.rs@hotmail.com>
2019-06-14 10:27:00 +0530

    Final changes

--- factorial_series.py
+++ factorial_series.py
@@ -1,9 +1,15 @@
 # This program prints factorial series
 # Function factorial series
-def factorial():
+def factorial(number):
+    fact = 1
+    # Factorial loop
+    for i in range(1, number+1):
+        fact = fact * i
+    return fact
 
-    return
+
+# Main Function
 
 if __name__ == "__main__":
     number  = int(input("Enter a number to find factorial >"))
-    print("Factorial series:",)
+    print("Factorial series:",factorial(number))
