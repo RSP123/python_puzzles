ed02261c8c2b9e51c2881bfd4303e0296d3b921c
rsp123 <pradeep.rs@hotmail.com>
2019-06-14 11:25:24 +0530

    Some changes in code

--- largest_list.py
+++ largest_list.py
@@ -2,17 +2,17 @@
 
 # Function for finding largest next_number
 
-def largest_list(list):
-    max =list[-1]
-    for i in range(i, i+1):
-        if i > max:
-            max = i
-    return
+def largest_list(list_larger):
+    max = list_larger[0]
+    for b in list_larger:
+        if b > max:
+            max = b
+    return max
 
 
 if __name__ == "__main__":
     element = int(input("Enter the element to find largest element in list :> "))
     for i in range(1, element+1):
-        a=int(input("Enter the number >:"))
-        list.append(a)
-    print(largest_list(list))
+        list_larger=list(map(int,input("Enter the number >:").split()))
+        # list.append(a)
+    print(largest_list(list_larger))
