6cb400f2473e328734c9a889dd09426ab41fb35a
rsp123 <pradeep.rs@hotmail.com>
2019-06-15 11:06:56 +0530

    changed some code

---
+++ read_text_file.py
@@ -0,0 +1,11 @@
+# This program reads text file
+# Print no. of sentence and words count
+
+#Function to read text file
+
+def read_text_file():
+    return
+
+# Main Function
+
+if __name__ == "__main__":
