564a74488bf25d85b99ee598860e387d66c6313c
rsp123 <pradeep.rs@hotmail.com>
2019-06-15 19:04:10 +0530

    Changes in code

--- read_text_file.py
+++ read_text_file.py
@@ -3,9 +3,17 @@
 
 #Function to read text file
 
-def read_text_file():
-    return
+def read_text_file(file_name):
+    with open('file_name','r') as new_file:
+        file_data = new_file.readline()
+        words_count = file_data.split()
+        words += len(words_count)
+    return words
 
 # Main Function
 
 if __name__ == "__main__":
+    file_name = input("Enter file name to read it >>>")
+
+    print("----------Selected file : ",file_name + "----------")
+    print("Number of words : ", read_text_file(file_name))
