df58a7539e0eb40d0ea1d5e0dea69d655b4039aa
rsp123 <pradeep.rs@hotmail.com>
2019-06-16 17:37:52 +0530

    Initial code

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+# This program prints panlindrom of given string
+
+# Function for panlindrom
+def panlindrom(p):
+
+    # Check for panlindrom
+    
+
+    return
+
+# Main Function
+if __name__=="__main__":
+    p = input("Enter the panlindrom to check : ")
+
+    print(panlindrom(p))
