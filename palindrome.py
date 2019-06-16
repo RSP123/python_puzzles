95af0c372ce6e5b389416bd34688fa2d65f77bd5
rsp123 <pradeep.rs@hotmail.com>
2019-06-16 18:13:55 +0530

    final changes

--- palindrome.py
+++ palindrome.py
@@ -4,16 +4,18 @@
 def palindrom(p):
 
     # Check for panlindrom
-    while (p != NULL):
-
-
-    return
+    while (p != 0):
+        original = p
+        reversed = p[::-1]
+        if original == reversed:
+            return True
+        return False
 
 # Main Function
 if __name__=="__main__":
     p = input("Enter the palindrom to check : ")
     check = palindrom(p)
-    if check():
+    if check:
         print("String is palindrom")
     else:
         print("String is not palindrom")
