d7af2b45fc106a20d1b0790540b506f8fc7aaff8
rsp123 <pradeep.rs@hotmail.com>
2019-06-18 11:48:33 +0530

    bugs found

--- uppercase_camelcase.py
+++ uppercase_camelcase.py
@@ -1,15 +1,24 @@
 # This program convert lowercase to uppercase and camelcase
 # Function for uppercase
-def upper_case(len(lower)):
-    upper = ord(lower[i]) >= 67
-    return upper
+def upper_case(str):
+    upper = ''
+    # Checks if char is lowercase then changes to uppercase
+    for char in str:
+        if ord(char) >= 65:
+            upper += chr(ord(char) - 32)
+            uppercase = upper.split()
+    return upper_case
+
+# Function for CamelCase
+def camel_case(lower):
+    camel = ''
+    text = ''
+    # Chaning to CamelCase
+    for char in lower:
+        camel = text[:1].upper()
 
 
-def camelcase(lower):
-    
-
-    return camel
-
+    return camel.upper()
 
 if __name__ == "__main__":
     lower = str(input("Enter a string here: >"))
