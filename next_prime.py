fc7aa09caf207ee87b37ccc15872cd084ac50328
rsp123 <pradeep.rs@hotmail.com>
2019-06-18 17:10:09 +0530

    bugs cleared

--- next_prime.py
+++ next_prime.py
@@ -1,28 +1,31 @@
 # This program prints next given prime number
 
-# MAin Function
+# Function for finding next prime number
+def is_prime(current_number):
+    limit = current_number * 2
+    # Current_number checking with prime Function
+    for i in range(current_number,limit):
+        if prime_number(i):
+            return i
 
+
+# Prime Function
+def prime_number(number):
+	# Returning False for number 1 and less then 1
+	if number == 2 or number == 1:
+		return True
+	elif number < 1:
+		return False
+	else:
+		# Iterating from 2 to check the number is prime or not
+		for i in range(3, int((number/3))+1, 2):
+			if (number%i) == 0:
+			    return False
+	return True
+
+
+
+# Main
 if __name__ == "__main__":
     current_number=int(input("Enter a prime number ")) + 1
-    if current_number == 0:
-        print("Oops!! You have entered zero")
-    else:
-        if current_number < 0:
-            print("Oops!! Enter positive value !!!")
-        else:
-            print("The next prime number is: ",is_prime(current_number))
-
-# Function for finding next prime number
-
-def is_prime(current_number):
-    if i in range(2, current_number):
-        if (number%i) == 0:
-            return False
-    return True
-
-# Function for finding prime number
-def next_prime(current_number):
-    next_number = current_number + 1
-    while is not next_prime(next_number):
-        next_number += 1
-    return next_number
+    print("The next prime number is: ",is_prime(current_number))
