0e1575c3330c7cf8cc48ecdfc1a32575bc0f730c
rsp123 <pradeep.rs@hotmail.com>
2019-06-18 12:31:45 +0530

    changed some lines of code

--- denomination.py
+++ denomination.py
@@ -2,15 +2,16 @@
 
 # Function for dinomination
 def denomi(amt):
-    d = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-    for i in d:
-        if amt >= d:
-            change = d
-            print(change)
+    d = list(map(int,[2000, 500, 200, 100, 50, 20, 10, 5, 2, 1]))
+    for i in range(len(d)):
+        if amt >= d[i]:
+            change = d[i]
+            print("amt -loop",change)
         else:
-            i += 1
+            d[i] = i + 1
+            print("else loop",d[i])
     return change
 
 if __name__ =="__main__":
     amt = 553
-    print("",denomi(amt))
+    print("result",denomi(amt))
